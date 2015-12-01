c648a6566a65a22eb9f8296313f71270804fb66a
Dima Stefantsov <dima@stefantsov.com>
2015-12-01 08:15:35 +0300

    Moved metrika to Snippets.

--- themes/momentous-lite-child/functions.php
+++ themes/momentous-lite-child/functions.php
@@ -294,57 +294,6 @@ add_action( 'widgets_init', 'child_momentous_register_sidebars' );
 
 wp_enqueue_script('child-momentous-lite-dindex-js', get_stylesheet_directory_uri() .'/js/index.js', array('jquery'));
 
-add_action('wp_footer','d_metrika');
-function d_metrika() {
-	if (function_exists('get_field') && get_field("hide_metrika")) {
-		return;
-	}
-
-    echo "
-
-    <script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-65975495-1', 'auto');
-	  ga('send', 'pageview');
-	</script>
-
-	".'
-
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript">
-        (function (d, w, c) {
-            (w[c] = w[c] || []).push(function() {
-                try {
-                    w.yaCounter31383978 = new Ya.Metrika({
-                        id:31383978,
-                        clickmap:true,
-                        trackLinks:true,
-                        accurateTrackBounce:true
-                    });
-                } catch(e) { }
-            });
-            var n = d.getElementsByTagName("script")[0],
-                s = d.createElement("script"),
-                f = function () { n.parentNode.insertBefore(s, n); };
-            s.type = "text/javascript";
-            s.async = true;
-            s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-            if (w.opera == "[object Opera]") {
-                d.addEventListener("DOMContentLoaded", f, false);
-            } else { f(); }
-        })(document, window, "yandex_metrika_callbacks");
-    </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/31383978" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-    <!-- /Yandex.Metrika counter -->
-
-    ';
-}
-
 // ACF Advanced Custom Fields
 add_action('wp_head','d_head');
 function d_head() {
